101fcf5989bc2f7d6ba3aed763cc41bbab02c926
rod641916 <tapiareyesrodrigo@gmail.com>
2022-04-15 13:06:38 -0500

    Nueva carpeta para operadrores y example 1

---
+++ weekly_mission_2/1_operadores/example1.js
@@ -0,0 +1,6 @@
+const number=[1,2,3,4,5,6,7,8,9,10]
+
+console.log("Ejemplo 1: Imprimir los ejemplos de una lista")
+number.forEach(num => console.log(num))
+
+//forEach sirve para imprimir elementos de una lista
