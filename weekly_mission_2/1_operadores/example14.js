b6d08354582187aa98d4a9b25d5e75ef0b049415
rod641916 <tapiareyesrodrigo@gmail.com>
2022-04-16 13:00:01 -0500

    Agredaor example 14-16 (finalizados los ejercicios)

---
+++ weekly_mission_2/1_operadores/example14.js
@@ -0,0 +1,6 @@
+const bools = [true, true, false, true];
+
+const areSomeTrue = bools.some((b) => b === false);
+
+console.log(
+  "Example 14: Alguno de los elementos en el array es false " + areSomeTrue);
---
+++ weekly_mission_2/1_operadores/example15.js
@@ -0,0 +1,4 @@
+const products = ["Milk", "Coffee", "Sugar", "Honey", "Apple", "Carrot"];
+
+console.log("Ejemplo 15: Elementos ordenados usando Sort");
+console.log(products.sort());
