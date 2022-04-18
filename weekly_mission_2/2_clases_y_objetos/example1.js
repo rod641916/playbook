3d98bef4423d46bdd5ac749afb02288d17cd33a2
rod641916 <tapiareyesrodrigo@gmail.com>
2022-04-18 12:29:24 -0500

    Se añade carpetas y ejercicios al la caprta 2 de clases y objetos

---
+++ weekly_mission_2/2_clases_y_objetos/example1.js
@@ -0,0 +1,5 @@
+class Person{
+}
+
+console.log("Ejemplo 1: Crear una clase vacia")
+console.log(Person)
---
+++ weekly_mission_2/2_clases_y_objetos/example2.js
@@ -0,0 +1,7 @@
+class Pet{
+
+}
+
+const myPet1=new Pet()
+console.log("Ejemplo 2: Crear un objeto a partir de una clase")
+console.log(myPet1)
