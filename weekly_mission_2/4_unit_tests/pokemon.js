725046042fbec60d28179d01ca05ccae517083e2
rod641916 <tapiareyesrodrigo@gmail.com>
2022-04-18 14:39:37 -0500

    todo el pryecto largo xd

---
+++ weekly_mission_2/4_unit_tests/pokemon.js
@@ -0,0 +1,19 @@
+export default class Pokemon {
+    constructor(name, type, age){
+      this.name = name
+      this.type = type
+      this.age = age
+      this.attacks = []
+    }
+  
+    get getAttacks(){
+      return this.attacks
+    }
+  
+    set setAttacks(attacks){
+      return this.attacks = attacks
+    }
+  }
+  
+  const myPokemon = new Pokemon("Charmander", "Fuego", 10)
+  console.log(myPokemon)
