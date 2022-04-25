b4520421bcabc80e77c488bacb657f25bb711aed
rod641916 <tapiareyesrodrigo@gmail.com>
2022-04-25 13:30:22 -0500

    Se agregaron nuevos archivos, proyecto desde cero

---
+++ weekly_mission_3/example1/app/missionCommander.js
@@ -0,0 +1,7 @@
+class MissionCommandder{
+    constructor (name){
+        this.name=name;
+    }
+}
+
+module.exports=MissionCommandder
---
+++ weekly_mission_3/example1/index.js
@@ -0,0 +1,4 @@
+const MissionCommandder=require('./app/missionCommander');
+
+const rodrigo=new MissionCommandder("Rodrigo")
+console.log(rodrigo.name)
